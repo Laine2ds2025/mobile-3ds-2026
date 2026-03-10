51229ba8bba6776d5e9a63a7d6c052c9f61920c8
Laine2ds2025 <ville.ferreira@escola.pr.gov.br>
2026-03-10 12:09:54 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,20 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Aula01 from "./src/aula01";
+import Aula02 from "./src/aula02";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    //<Aula01 />
+    <Aula02 />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
