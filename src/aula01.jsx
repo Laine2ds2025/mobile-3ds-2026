d4dda7c4c2d77183772bc268e38df901887b1905
Laine2ds2025 <ville.ferreira@escola.pr.gov.br>
2026-03-19 09:18:56 -0300

    Add files via upload

--- src/aula01.jsx
+++ src/aula01.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 
 class Aula01 extends Component {
     render(){
